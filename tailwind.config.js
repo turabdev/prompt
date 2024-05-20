6e756910881fc6d10b90a5a1ef7f1d1086664067
turabdev <turabzaidi1971@gmail.com>
2024-05-20 20:17:46 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        vietnam: ["be vietnam pro", "san-serif"],
+      },
+      colors: {
+        primary: "#06b6d4",
+      },
+    },
+  },
+  plugins: [],
+};
